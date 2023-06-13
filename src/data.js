aca628ff67901ea2ac16fb249e60ff3c6ecc231f
Zionnaıre <zionnaire2018@gmal.com>
2023-06-13 07:59:40 +0100

    Multimedia-app

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+      id: 1,
+      name: 'Video File',
+      type: 'video',
+      path: "/file-server/baking.mp4"
+    },
+    {
+      id: 2,
+      name: 'Audio File',
+      type: 'audio',
+      path: "/file-server/Joeboy-Woman.mp3"
+    },
+    {
+      id: 3,
+      name: 'Document File',
+      type: 'document',
+      path: "/file-server/out.pdf"
+    },
+    {
+      id: 4,
+      name: 'Image File',
+      type: 'image',
+      path: "/file-server/carnival.jpg"
+    },
+    {
+        id: 5,
+        name: 'Image File',
+        type: 'image',
+        path: "/file-server/Pieta.jpg"
+      },
+      {
+        id: 6,
+        name: 'Image File',
+        type: 'image',
+        path: "/file-server/Picasso.jpg"
+      },
+  ]
